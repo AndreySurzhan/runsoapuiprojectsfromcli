b18eb4cca3321d4c6fc439463db0826e3c00c169
Andrey Surzhan <surzhan.a.y@gmail.com>
2017-09-04 17:17:00 -0400

    Added possibility to exclude suites and projects
    - refactoring
    - possibility to run only one test suite
    - added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+-s - native soapUI cli arguments. Hoverer with current script it is possible to specify group of test suites
+ using `*` at the end of the name
+--prog
+--excludesuites - list of test suites to exclude
+--excludeprojects - list of projects to exclude
--- soapuirunner.py
+++ soapuirunner.py
@@ -6,50 +6,80 @@ import sys
 import xml.etree.ElementTree as ET
 from smtpd import usage
 
+
+CUSTOM_ARGS = [
+    'prog',
+    'excludesuites',
+    'excludeprojects'
+]
+
+
 def get_list_of_soapui_projects(root_folder):
     soapui_projects = []
-
-    for (root_path, dirnames, filenames) in walk(root_folder):
-        if filenames:
-            for filename in filenames:
-                filename_without_extension, file_extension = path.splitext(filename)
-                if file_extension == '.xml':
-                    full_file_path = path.join(root_path, filename)
-                    xml_root_tag = ET.parse(full_file_path).getroot().tag
-                    # figure out that xml is soapUI project file
-                    if 'soapui-project' in xml_root_tag:
+    if path.isfile(root_folder):
+        if is_soapui_project_file(root_folder):
+            return soapui_projects.append(root_folder)
+    if path.isdir(root_folder):
+        for (root_path, dirnames, filenames) in walk(root_folder):
+            if filenames:
+                for filename in filenames:
+                    full_file_path = path.join(root_folder, filename)
+                    if is_soapui_project_file(full_file_path):
                         soapui_projects.append(full_file_path)
-
-        if dirnames:
-            for dirname in dirnames:
-                soapui_projects.extend(get_list_of_soapui_projects(dirname))
-
+            if dirnames:
+                for dirname in dirnames:
+                    soapui_projects.extend(get_list_of_soapui_projects(dirname))
     return soapui_projects
+
+
+def build_list_of_soapui_projects(project_list, exclude=None):
+    if exclude:
+        exclude = exclude.split(',')
+        for project_to_exclude in exclude:
+            dirname = path.dirname(project_list[0])
+            project_to_exclude = path.join(dirname, project_to_exclude)
+            if project_to_exclude in project_list:
+                project_list.pop(project_list.index(project_to_exclude))
+    return project_list
+
+
+def is_soapui_project_file(full_file_path):
+    filename_without_extension, file_extension = path.splitext(full_file_path)
+    if file_extension == '.xml':
+        xml_root_tag = ET.parse(full_file_path).getroot().tag
+        # figure out that xml is soapUI project file
+        if 'soapui-project' in xml_root_tag:
+            return True
+    return False
 
 
 def get_list_of_suites_from_soapui_project(soapui_project_path):
     root = ET.parse(soapui_project_path).getroot()
     test_suites = []
-
     for child in root:
         if 'testSuite' in child.tag:
             test_suites.append(child.attrib['name'])
-
     return test_suites
 
 
-def build_list_of_suites(test_suite, suites_list):
+def build_list_of_suites(test_suite, suites_list, exclude=None):
     if test_suite.endswith('*'):
-        return [suite for suite in suites_list if test_suite[:-1] in suite]
-
-    return [test_suite]
+        suites_list = [suite for suite in suites_list if test_suite[:-1] in suite]
+        if exclude:
+            exclude = exclude.split(',')
+            for suite_to_exclude in exclude:
+                if suite_to_exclude in suites_list:
+                    suites_list.pop(suites_list.index(suite_to_exclude))
+        return suites_list
+    if test_suite in suites_list:
+        return [test_suite]
+    return None
 
 
 def get_option_value(opts_list, opt_name):
     for opt in opts_list:
         if opt[0] == opt_name:
             return opt[1]
-
     return None
 
 
@@ -62,7 +92,6 @@ def set_option_value(opts_list, opt_name, opt_value):
             opt = tuple(opt)
             opts_list.pop(i)
             opts_list.insert(i, opt)
-
     return opts_list
 
 
@@ -80,7 +109,6 @@ def get_platform_name():
             'windows'
         ]
     }
-
     for platform_type, platform_names in platform_types.items():
         for name in platform_names:
             if name == platform_name:
@@ -95,34 +123,27 @@ def build_string_of_options(argv):
             option = option[1:2]
         else:
             option = option[1:2] + ':'
-
         if options == '':
             options = option
         else:
             options = options + option
-
     return options
 
 
-def build_command_to_run_soapui(soapui_project_path, args, program=None,):
+def build_command_to_run_soapui(soapui_project_path, args, program=None):
     # setup default program to run soapUI from cli
     if program is None:
         platform_name = get_platform_name()
-
         if platform_name == 'unix':
             program = 'testrunner.sh'
         if platform_name == 'win':
             program = 'testrunner.bat'
-
     command = [program]
-    # '/Applications/SoapUI-5.2.1.app/Contents/Resources/app/bin/testrunner.sh'
-
     if args is not None:
-        for arg in args[1:]:
-            command.append(''.join(arg))
-
+        for arg in args:
+            if arg[0].replace('--', '') not in CUSTOM_ARGS:
+                command.append(''.join(arg))
     command.append(soapui_project_path)
-
     return command
 
 
@@ -130,29 +151,31 @@ def main(argv):
     soapui_root_dir = argv[-1:][0]
     options = build_string_of_options(argv)
     result = []
-
     try:
-        opts, args = getopt.getopt(argv[:-1], options, ['prog='])
+        opts, args = getopt.getopt(argv[:-1], options, [arg + '=' for arg in CUSTOM_ARGS])
     except getopt.GetoptError as err:
         print(str(err))
         sys.exit(2)  # cli syntax error
-
     # get test suite name from arguments
-    test_suite_name = get_option_value(opts, '-s')
-    soapip_projects_list = get_list_of_soapui_projects(soapui_root_dir)
-
-    for soapui_project in soapip_projects_list:
+    original_test_suite_name = get_option_value(opts, '-s')
+    projects_to_exclude = get_option_value(opts, '--excludeprojects')
+    suites_to_exclude = get_option_value(opts, '--excludesuites')
+    soapui_projects_list = get_list_of_soapui_projects(soapui_root_dir)
+    soapui_projects_list = build_list_of_soapui_projects(soapui_projects_list, projects_to_exclude)
+    for soapui_project in soapui_projects_list:
         # if test suite has * at the end of the name that means we need to run all test suite started with name
+        test_suite_name = original_test_suite_name
         test_suite_names = get_list_of_suites_from_soapui_project(soapui_project)
-        test_suite_names = build_list_of_suites(test_suite_name, test_suite_names)
-        for test_suite_name in test_suite_names:
-            # run soapui project with params
-            opts = set_option_value(opts, '-s', test_suite_name)
-            program = get_option_value(opts, '--prog')
-            command_to_run = build_command_to_run_soapui(soapui_project, opts, program)
-            print(command_to_run)
-            #result.append(call(command_to_run))
-
+        test_suite_names = build_list_of_suites(test_suite_name, test_suite_names, suites_to_exclude)
+        if test_suite_names:
+            for test_suite_name in test_suite_names:
+                # run soapui project with params
+                set_option_value(opts, '-s', test_suite_name)
+                program = get_option_value(opts, '--prog')
+                command_to_run = build_command_to_run_soapui(soapui_project, opts, program)
+                print(command_to_run)
+                #result.append(call(command_to_run))
+                set_option_value(opts, '-s', original_test_suite_name)
     if all(i > 0 for i in result):
         sys.exit(1)  # if something fails
 
@@ -163,9 +186,6 @@ if __name__ == "__main__":
 """
 TODO:
 
-- possible to run only one project
-- possible to run only one test suite
-- possible to exclude project
-- possible to exclude test suite
 - readme
+- docs
 """
